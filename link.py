023f6dc835e38e40e6f1353ccf5699fb141c39b1
daniaokuye <rs_lidn@163.com>
2017-11-17 22:07:15 +0800

    the main function & get *.xml and *.jpg to feed the transformation

--- link.py
+++ link.py
@@ -1,42 +1,44 @@
 #
 import os
-from xmlSet import gotXMLInfo
+from xmlSet import mainFunction
 from Transform import *
 from scipy.misc import imread
 
 
-
 def linkImgAnn(cp):
-    data,ann=cp
-    dataDir='/'.join( data.split('/')[0:2] )
-    annDir='/'.join( ann.split('/')[0:2] )
-    extend='_Aug'
+    data, ann = cp
+    dataDir = '/'.join(data.split('/')[0:2])
+    annDir = '/'.join(ann.split('/')[0:2])
+    extend = '_Aug'
 
 
 def readFile():
-    testFile='data/test.txt'
+    testFile = 'data/test.txt'
 
     for bond in open(testFile):
-        both=bond.split()
+        both = bond.split()
         yield both
         '''
         if os.path.exists(both[0]) and os.path.exists(both[1]) :
             linkImgANN(both)
         '''
 
-def readAnnoImage():
-    info="/home/flag54/Documents/dataSetAugument/data/anno/000001.xml"
-    photo = "/home/flag54/Documents/dataSetAugument/data/dataSet/000001.jpg"
-    print os.curdir
-    #cp = readFile()
-    #read xmlAnno
-    objects = gotXMLInfo(info)
-    #read img
-    image =imread(photo)
-    #get transformed image
-    dict_trans = transform(image,objects)
 
-    return  dict_trans
+def readAnnoImage():
+    prex = "/home/flag54/Documents/dataSetAugument/"
+
+    # read xmlAnno
+    couples = readFile()
+    for cp in couples:
+        try:
+            img, anno = cp
+            data, labels = mainFunction(prex + img, prex + anno)
+
+            ###deal with data & its labels
+            show_data(data, labels)
+        except Exception, e:
+            print e
+
 
 if __name__ == "__main__":
     readAnnoImage()

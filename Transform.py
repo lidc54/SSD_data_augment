43d06e024b500e189af8b39351c95d340807c554
daniaokuye <rs_lidn@163.com>
2017-11-17 22:04:33 +0800

    this is the function which to call other ones to transform

--- Transform.py
+++ Transform.py
@@ -5,46 +5,50 @@ expand:origin image expand to 4*4
 crop:random crop
 '''
 from jaccard import *
-#import random
-#from PIL import Image, ImageEnhance, ImageOps, ImageFile
-#import matplotlib.pyplot as plt
-#import numpy as np
-#import copy
 
 
-def transform(image,objects):
+# import random
+# from PIL import Image, ImageEnhance, ImageOps, ImageFile
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import copy
+
+
+def transform(image, objects):
     '''data augment'''
-    trans_dict={}
+    trans_dict = {}
     sz = (300, 300)  # width & height -- order
 
     # 1.expand image
-    trans_dict['expand'] = expand(image,objects,sz)
+    trans_dict['expand'] = expand(image, objects, sz)
 
     # 2.origin image -- resize
-    image, objects = resize_imgAnno(sz,image,objects)
+    image, objects = resize_imgAnno(sz, image, objects)
     trans_dict['origin'] = [image.copy(), copy.deepcopy(objects)]
 
     # 3.mirror
-    prob = random.randint(1,2)
-    if prob >1:
-        image = image[:,::-1]
-        mirrot_anno(image,objects)
-        trans_dict['mirror'] = [image.copy(),copy.deepcopy(objects)]
+    prob = random.randint(1, 2)
+    if prob > 1:
+        image = image[:, ::-1]
+        mirrot_anno(image, objects)
+        trans_dict['mirror'] = [image.copy(), copy.deepcopy(objects)]
 
     # 4.distort
     image = jitter(image)
-    #trans_dict['distort'] = [image.copy(),copy.deepcopy(objects)]
+    # trans_dict['distort'] = [image.copy(),copy.deepcopy(objects)]
 
     # 5.jaccard
-    dict_jaccard = corp_image(image,objects,sz)
-    trans_dict = dict(trans_dict,**dict_jaccard)
-    print 'po'
+    try:
+        dict_jaccard = corp_image(image, objects, sz)
+        trans_dict = dict(trans_dict, **dict_jaccard)
+        print 'po'
+    except Exception, e:
+        print e
 
-    return  trans_dict
+    return trans_dict
 
 
-
-def mirrot_anno(image,objects):
+def mirrot_anno(image, objects):
     '''mirror the annoated info at the same time'''
     height, width, channel = image.shape
     coord = ['xmin', 'xmax', 'ymin', 'ymax']
@@ -54,6 +58,7 @@ def mirrot_anno(image,objects):
         new_xmax = width - xmin
         objects[i]['bndbox']['xmin'] = new_xmin
         objects[i]['bndbox']['xmax'] = new_xmax
+
 
 def jitter(data):
     image = Image.fromarray(data)
@@ -71,32 +76,32 @@ def jitter(data):
     '''
     return np.array(contrast_image)
 
-def expand(image,objects,sz,ratio=3):
+
+def expand(image, objects, sz, ratio=3):
     '''zoom out'''
     height, width, channel = image.shape
-    aug_sz=(sz[0]*ratio, sz[1]*ratio)
-    mean_value = [104,117,123]
-    #build a canvus
-    canvus = np.zeros((aug_sz[0],aug_sz[0],channel),dtype="uint8")
+    aug_sz = (sz[0] * ratio, sz[1] * ratio)
+    mean_value = [104, 117, 123]
+    # build a canvus
+    canvus = np.zeros((aug_sz[0], aug_sz[0], channel), dtype="uint8")
     for i in range(channel):
-        canvus[:,:,i] = mean_value[i]
+        canvus[:, :, i] = mean_value[i]
 
-    #insert the image
+    # insert the image
     h_off = random.randint(0, aug_sz[0] - height)
     w_off = random.randint(0, aug_sz[1] - width)
-    canvus[h_off:h_off+height,w_off:w_off+width,:] = image
+    canvus[h_off:h_off + height, w_off:w_off + width, :] = image
 
-    #adjust the labels
+    # adjust the labels
     new_objects = copy.deepcopy(objects)
     coord = ['xmin', 'xmax', 'ymin', 'ymax']
     for i in new_objects.keys():
         coor = new_objects[i]['bndbox']
         xmin, xmax, ymin, ymax = [int(coor[k]) for k in coord]  # coor's coordination is num now,not a string
-        newCoor = [xmin+w_off, xmax+w_off, ymin+h_off, ymax+h_off]
-        for k,key in enumerate(coord):
+        newCoor = [xmin + w_off, xmax + w_off, ymin + h_off, ymax + h_off]
+        for k, key in enumerate(coord):
             coor[key] = newCoor[k]
 
-    canvus,new_objects = resize_imgAnno(sz,canvus,new_objects)
+    canvus, new_objects = resize_imgAnno(sz, canvus, new_objects)
 
-    return  canvus,new_objects
-
+    return canvus, new_objects
